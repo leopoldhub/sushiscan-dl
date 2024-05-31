496d413bbe48c21ecc9ed2ddb72edcf09b58e79e
SeanR-ScanR <scanr.seanr@gmail.com>
2024-05-31 23:10:18 +0200

    Fixed + BAR

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Cookie, firefox } from 'playwright';
 import fs from 'fs';
 import fsExtra from 'fs-extra';
 import minimist from 'minimist';
+import cliProgress from 'cli-progress';
+import ansiColors from 'ansi-colors';
 
 const COOKIES_FILE_PATH = './cookies.json';
 
@@ -10,7 +12,10 @@ function loadSavedCookies() {
   if (fs.existsSync(COOKIES_FILE_PATH)) {
     existingCookies.push(
       ...JSON.parse(
-        fs.readFileSync(COOKIES_FILE_PATH, { encoding: 'utf8', flag: 'r' })
+        fs.readFileSync(COOKIES_FILE_PATH, {
+          encoding: 'utf8',
+          flag: 'r',
+        })
       )
     );
   }
@@ -40,9 +45,7 @@ async function preloadCookies(url: string, existingCookies: Cookie[]) {
     if (!initial) {
       initial = true;
       console.log('is in cloudflare!');
-      console.log(
-        'complete the captcha or close the program and manually import valid cookies in the "cookies.json" file.'
-      );
+      console.log('complete the captcha or close the program and manually import valid cookies in the "cookies.json" file.');
     }
     await page.waitForURL('https://sushiscan.net/**', {
       waitUntil: 'domcontentloaded',
@@ -54,83 +57,73 @@ async function preloadCookies(url: string, existingCookies: Cookie[]) {
   return cookies;
 }
 
-async function downloadingSushiscanPageImages(
-  url: string,
-  destinationFolder: string
-) {
+async function downloadingSushiscanPageImages(url: string, destinationFolder: string) {
   console.log('preloading cookies');
   const preloadedCookies = await preloadCookies(url, loadSavedCookies());
   console.log('saving loaded cookies');
   saveCookies(preloadedCookies);
 
-  console.log('launching browser');
-  const browser =
-    await firefox.launch(/*{
-    headless: false,
-    args: ['-devtools'],
-  }*/);
+  const browser = await firefox.launch({
+    // headless: false,
+    // args: ['-devtools'],
+  });
 
-  console.log('loading context...');
   const context = await browser.newContext();
   await context.addCookies(preloadedCookies);
   const page = await context.newPage();
-  await page.setViewportSize({ width: 1920, height: 1080 });
+  await page.setViewportSize({
+    width: 1920,
+    height: 1080,
+  });
   await page.addInitScript(() => {
     localStorage.setItem('tsms_readingmode', '"full"');
   });
 
-  console.log(
-    'loading page... (waiting for the page to stabilize, might take up to a minute)'
+  console.log('loading page... (waiting for the page to stabilize, might take up to a minute)\n');
+
+  const bar1 = new cliProgress.SingleBar(
+    {
+      barCompleteChar: ansiColors.magenta('#'),
+      barIncompleteChar: '.',
+      fps: 5,
+      stream: process.stdout,
+      barsize: 65,
+    },
+    cliProgress.Presets.shades_classic
   );
+  const urlPassed: string[] = [];
+  let taille: number;
+  page.on('response', async (response) => {
+    if (response.ok() && response.url() == (url.endsWith('/') ? url : url + '/')) {
+      const [_, images] = (await response.text()).match(/"images":\s?\[([^\]]*)\]/) as RegExpMatchArray;
+      taille = images.split(',').length;
+      bar1.start(taille, 0);
+    }
+    if (response.ok() && response.url().match(/wp-content\/upload.+-\d+\.\w+$/) && !urlPassed.includes(response.url())) {
+      urlPassed.push(response.url());
+      const [_, folder, name] = response.url().match(/\/([^/-]+)-(\d+)\.\w+$/) as RegExpMatchArray;
+      fsExtra.outputFileSync(`${destinationFolder}/${folder}/${name}.jpg`, await response.body());
+      bar1.increment();
+      if (urlPassed.length === taille) {
+        bar1.stop();
+        console.log('\nDownload complete!');
+      }
+    }
+  });
   await page.goto(url, {
     waitUntil: 'networkidle',
     timeout: 120000,
   });
 
-  console.log('rendering images...');
   await (
     await page.locator('#readerarea>img').all()
   ).reduce<Promise<any>>(async (previousValue, l) => {
-    console.log('loading image', await l.getAttribute('data-index'));
     return previousValue
       .then(() => l.evaluate((img: any) => img.scrollIntoView()))
       .then(() => page.waitForLoadState('networkidle'));
   }, Promise.resolve());
   await page.waitForLoadState('networkidle');
-
-  console.log('extracting images...');
-  await Promise.all(
-    (await page.locator('#readerarea>img').all()).map(async (l) => {
-      const imageUrl = await l.getAttribute('data-index');
-      console.log('extracting image', imageUrl);
-      const imageData = await l.evaluate((img: any) => {
-        const c = document.createElement('canvas');
-        c.height = img.naturalHeight;
-        c.width = img.naturalWidth;
-        const ctx: any = c.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-        return {
-          data: c.toDataURL(),
-          extension: new URL(img.getAttribute('src')).pathname.split('.').pop(),
-          index: parseInt(img.getAttribute('data-index')),
-        };
-      });
-      console.log('downloading image', imageUrl);
-      const formatedData = imageData.data.replace(/^data:.+;base64,/, '');
-      fs.promises
-        .writeFile(
-          destinationFolder + '/' + imageData.index + '.' + imageData.extension,
-          formatedData,
-          { encoding: 'base64', flag: 'w' }
-        )
-        .catch((e) => {
-          console.error('failed to download image ' + imageData.index);
-          console.trace(e);
-        });
-    })
-  );
-
-  console.log('download complete!');
+  console.log('End of treatment');
   await browser.close();
 }
 
